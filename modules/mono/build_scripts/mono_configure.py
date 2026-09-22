7097aa5bd37e190a56a7fa4f0321dd4e95c63592
gasukinani <gasukinani80@gmail.com>
2026-09-23 00:28:41 +0800

    Update mono_configure.py

--- modules/mono/build_scripts/mono_configure.py
+++ modules/mono/build_scripts/mono_configure.py
@@ -7,20 +7,11 @@ def is_unix_like(platform):
 
 
 def module_supports_tools_on(platform):
-    # PAYAGAN ANG ANDROID DITO:
+    # AYOS: Pinapayagan ang android para sa tools/editor build
     return is_desktop(platform) or platform == "android"
 
 
 def configure(env, env_mono):
-    # is_android = env["platform"] == "android"
-    # is_web = env["platform"] == "web"
-    # is_ios = env["platform"] == "ios"
-    # is_ios_sim = is_ios and env["arch"] in ["x86_32", "x86_64"]
-
     if env.editor_build:
-        # Siguraduhing kasama ang android sa allowed platforms:
         if env["platform"] not in ["windows", "macos", "linuxbsd", "android"]:
             raise RuntimeError("This module does not currently support building for this platform for editor builds.")
-
-    # Tiyakin na kung Android, hindi ito maghahanap ng desktop-specific hostfxr static libraries
-    # dahil dynamically loaded ito via dlopen sa gd_mono.cpp mo.
